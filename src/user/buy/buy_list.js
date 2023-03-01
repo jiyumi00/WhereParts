4d1b39e7bb6f932800a652ee609cdd5ed5bbc4ff
DESKTOP-AI6RRTR\user <jiyumi00@naver.com>
2023-03-01 17:43:22 +0900

    add project

---
+++ src/user/buy/buy_list.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react';
+import { View,Text,ScrollView,TouchableOpacity,Modal,FlatList,Image } from 'react-native';
+
+import { template } from "../../styles/template/page_style";
+import {styles} from "../../styles/buylist";
+import Constant from '../../util/constatnt_variables';
+import WebServiceManager from '../../util/webservice_manager';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+
+class BuyList extends Component {
+    constructor(props) {
+        super(props);
+        this.userID=""
+        this.state={
+            buyContents:[],
+        }
+    }
+    componentDidMount() {
+        this.getUserID().then(()=>{
+        this.callGetGoodsAPI().then((response) => { 
+           this.setState({buyContents:response})
+        });
+    })
+    }
+    async getUserID(){
+        let obj=await AsyncStorage.getItem('obj')
+        let parsed=JSON.parse(obj);
+        if(obj!==null){
+            this.userID=parsed.id;
+        }
+        else{
+            return false;
+        }
+    }
+    //등록된 상품 리스트 API
+    async callGetGoodsAPI() {
+        let manager = new WebServiceManager(Constant.serviceURL + "/GetOrders?id="+this.userID);
+        let response = await manager.start();
+        if (response.ok)
+            return response.json();
+        else
+            Promise.reject(response);
+    }
+   
+    render() {
+        {console.log(this.state.buyContents)}
+        return ( 
+           <View style={{flex:1, marginBottom:10,}}>
+                <FlatList
+                    data={this.state.buyContents}
+                    renderItem={({ item, index }) => <ListItem index={index} item={item} goodsID={item.goodsID} id={item.id}navigation={this.props.navigation} />}
+                    scrollEventThrottle={16}
+                />
+           </View>
+        );
+    }
+}
+export default BuyList;
+
+class ListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            id:"",
+            imageURI: null,
+            isDetailViewModal: false,
+        };
+    }
+
+    componentDidMount() {
+        
+        this.callGetImageAPI().then((response) => {
+            let reader = new FileReader();
+            reader.readAsDataURL(response); //blob을 읽어줌 읽은 놈이 reader
+            reader.onloadend = () => {
+                
+                this.setState({ imageURI: reader.result }) //base64를 imageURI에 집어넣어준다
+
+            } //끝까지 다 읽었으면 
+        });
+    }
+    async callGetImageAPI() {
+            let manager = new WebServiceManager(Constant.serviceURL + "/GetGoodsImage?id=" + this.props.goodsID + "&position=1");
+            let response = await manager.start();
+            if (response.ok)
+                return response.blob();
+    }
+    goDeliveryDetailScreen=()=>{
+        const logisInfo={code:"04",invoice:"651969374875"};
+        this.props.navigation.navigate('DeliveryDetail',{logisInfo:logisInfo});
+    }
+    goOrderDetailScreen=()=>{
+        this.props.navigation.navigate('OrderDetail',{id:this.props.id})
+    }
+    handleDetailViewModal=()=> {
+        this.props.navigation.navigate('GoodsDetail', { id:this.props.goodsID, userID:this.props.item.userID });
+    }
+    goodsStatusText = (value) => {
+        let goodsStatusText = ["배송준비중", "배송중", "배송완료"];
+        return goodsStatusText[value - 1];
+    }
+    render() {
+        const item = this.props.item;
+        return (
+            <>
+     
+            <TouchableOpacity onPress={this.handleDetailViewModal}>
+             <View style={styles.itemView}>
+                    <View style={styles.dateView}>
+                        <Text>주문일  </Text><Text style={styles.itemRegisterDateText}>{item.orderingDate.slice(2,10)}</Text>
+                    </View>
+                    <View style={styles.productView}>
+                        <View style={styles.productImageView}>
+                           
+                            <Image
+                                source={{ uri: this.state.imageURI }}
+                                style={styles.productImage}/>
+                        </View>
+                        <View style={styles.productInfo}>
+                            <View style={styles.productInfoLeft}>
+                                
+                                <Text style={styles.itemNameText}>{item.goodsName}</Text>
+                                <Text style={styles.itemPriceText}>{item.total.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}{"원"} <Text style={styles.text}> / {item.quantity}개 </Text></Text>
+                                <Text style={styles.itemNumberText}>{item.goodsNo}</Text>
+                                
+                            </View>
+                            <View style={styles.productInfoRight}>
+                                <View style={styles.productDistance}>
+                                    <Text style={styles.itemDistanceText}>{this.goodsStatusText(item.status)}</Text> 
+                                </View>
+                               
+                            </View>                 
+                        </View>
+                    </View>
+                    <View style={styles.productButtonView}>
+                        <View style={styles.payInfoButtonView}>
+                            <TouchableOpacity onPress={this.goOrderDetailScreen}><Text>주문상세</Text></TouchableOpacity>
+                        </View>
+                        <View style={styles.deliverInfoButtonView}>
+                            <TouchableOpacity  onPress={this.goDeliveryDetailScreen}><Text>배송조회</Text></TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </TouchableOpacity>
+               
+           </>
+        );
+    }
+}
