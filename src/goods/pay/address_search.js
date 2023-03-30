fe5005df42a0bade944bc2b837ccc93c6d0db979
DESKTOP-AI6RRTR\user <jiyumi00@naver.com>
2023-03-30 23:59:50 +0900

    주소검색 코드 리팩토링

--- src/goods/pay/address_search.js
+++ src/goods/pay/address_search.js
@@ -10,207 +10,142 @@ import WebServiceManager from '../../util/webservice_manager';
 import Indicator from '../../util/indicator';
 const ScreenHeight=Dimensions.get('window').height;
 const ScreenWidth=Dimensions.get('window').width;
+
 class SearchAddress extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            modal: false,
-            addressName: "",
+            addressContents:[],
+            commonContents:[],
             searchText: "",
-            value:"",
-            
+            searchViewVisible:false,
+            page:1,
+            totalCount:0,
+            indicator:false,
         }
     }
 
     //검색 버튼을 눌렀을 때
-    searchAddress = () => {
+    searchAddress=()=>{
         if(this.state.searchText == "")
         {
             alert("주소를 입력해주세요");
         }
         else{
-            this.setState({value: this.state.searchText,modal:true});
+            this.goGetAddress();
+            this.setState({searchViewVisible:true});
             Keyboard.dismiss();
         }
     }
-    pageUp = () => {
-        this.setState({ page: this.state.page + 1 });
-    }
-    pageDown = () => {
-        this.setState({ page: this.state.page - 1 });
-    }
-
-    addressOnEndEditing=()=>{
-        if(this.state.searchText==""){
-            alert("주소를 입력해주세요");
-        }
-        else{
-            this.setState({value: this.state.searchText,modal:true})
-        }
-    }
-
-    render() {
-        return (
-            <View style={styles.total_container}>
-               
-                <View style={styles.container}>
-                    <View style={styles.viewHeader}>
-                        <View style={styles.inputStyle}>
-                            <View style={styles.rowLayout}>
-                                <TextInput style={styles.input}
-                                    onChangeText={(text) => this.setState({ searchText: text  })}
-                                    onEndEditing={this.addressOnEndEditing }
-                                    placeholder="도로명 또는 지번을 입력하세요"
-                                    placeholderTextColor="light grey" />
-                                <TouchableOpacity style={styles.search} onPress={this.searchAddress}>
-                                    <Icon name="search" size={30} color="light grey" />
-                                </TouchableOpacity>
-                            </View>
-                        </View>
-                    </View>
-                        {this.state.modal == false && (<AddressView />)}
-                        {this.state.modal == true && (<SearchView searchText={this.state.value} navigation={this.props.navigation} addressListener={this.props.route.params.addressListener} />)}
-
-                </View>
-            </View>
-        );
-    }
-}
-
-class AddressView extends Component {
-    constructor(props) {
-        super(props);
-
-    }
-    render() {
-        return (
-            <>
-       
-              <View style={[styles.viewBody,{position:'absolute',marginTop:'20%'}]}>
-                <Text style={styles.title}>TIP</Text>
-                <Text style={styles.text}>도로명, 건물명, 지번 중 선택하여</Text>
-                <Text style={styles.text2}>입력하세요 </Text>
-                <Text style={styles.content}> 도로명 + 건물번호 <Text style={styles.content2}> 예) 테헤란로 152</Text></Text>
-                <Text style={styles.content}> 동/읍/면/리 + 번지 <Text style={styles.content2}> 예) 역삼동 737</Text> </Text>
-                <Text style={styles.content}> 건물명, 아파트명  <Text style={styles.content2}> 예) 삼성동 힐스테이트</Text></Text>
-                </View>
-               
-            </>
-        );
-    }
-}
-
-class SearchView extends PureComponent {
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            addressList: [],
-            commonList: [],
-            page: 1, 
-            totalCount:0,
-            indicator: false,
-        }
-    }
-    componentDidMount() {
-        console.log('componentDidMount 실행')
+    goGetAddress=()=>{
         this.setState({indicator:true})
         this.callGetAddressAPI().then((response) => {
-            
-            this.setState({ addressList: response.results.juso,commonList: response.results.common,totalCount:response.results.common.totalCount, indicator:false });
+            this.setState({ addressContents: response.results.juso,commonContents: response.results.common,totalCount:response.results.common.totalCount,indicator:false });
             console.log('componentResponse',response)
         });
     }
-    componentDidUpdate(prevProps, prevState) {
-        console.log('[prevProps]',prevProps)
-        console.log('[PrevState]',prevState)
-        
-        if((prevState.page != this.state.page))
-        {
-            //this.setState({indicator:true})
-            this.callGetAddressAPI().then((response) => {
-                this.setState({ addressList: response.results.juso,commonList: response.results.common,totalCount:response.results.common.totalCount });
-            });
-        }
-        if(prevProps.searchText != this.props.searchText)
-        {
-            this.setState({indicator:true})
-            this.callGetAddressAPI().then((response) => {
-                this.setState({page:1, addressList: response.results.juso,commonList: response.results.common,totalCount:response.results.common.totalCount,indicator:false });
 
-            });
-           
-        }
+    pageDownClicked=()=>{
+        if (this.state.page > 1)
+            this.setState({ page: this.state.page - 1 },()=>this.goGetAddress())
     }
-
-    addressListClicked=(zipNo,roadAddr)=>{
-        //console.log("addressList", zipNo + roadAddr);
-        this.props.navigation.navigate('Payment');
-        this.props.addressListener(zipNo, roadAddr);
+    pageUpClicked=()=>{
+        if (this.state.page < (this.state.totalCount / 4))
+            this.setState({ page: this.state.page + 1 },()=>this.goGetAddress())
     }
-
     async callGetAddressAPI() {
-        let manager = new WebServiceManager("https://business.juso.go.kr/addrlink/addrLinkApi.do?confmKey=devU01TX0FVVEgyMDIzMDIwOTE3MzczMjExMzQ5Njg=&currentPage=" + this.state.page + "&countPerPage=4&keyword=" + this.props.searchText + "&resultType=json");
+        let manager = new WebServiceManager("https://business.juso.go.kr/addrlink/addrLinkApi.do?confmKey=devU01TX0FVVEgyMDIzMDIwOTE3MzczMjExMzQ5Njg=&currentPage=" + this.state.page + "&countPerPage=4&keyword=" + this.state.searchText + "&resultType=json");
         let response = await manager.start();
         if (response.ok)
             return response.json();
         else
             Promise.reject(response);
     }
-    //페이지 증가 
-    pageUp = () => {
-        if (this.state.page < (this.state.commonList.totalCount / 4))
-            this.setState({ page: this.state.page + 1 })
-    }
-    //페이지 감소 
-    pageDown = () => {
-        if (this.state.page > 1)
-            this.setState({ page: this.state.page - 1 })
-    }
     render() {
+        console.log('searchText',this.state.searchText)
         return (
-            <>
-            <Modal transparent={true} visible={this.state.indicator}>
-                    <Indicator />
-                </Modal>
-            <View style={[styles.viewBody,{position:'absolute',marginTop:'20%',width:'100%'}]}>
-            <FlatList
-                data={this.state.addressList}
-                renderItem={( {item} ) =><TouchableOpacity activeOpacity={0.8} onPress={()=>this.addressListClicked(item.zipNo,item.roadAddr)}>
-                    <View style={styles.outputStyle}>
-                    <View style={styles.rowLayout}>
-                        <View style={styles.titleLayout}>
-                            
-                            <View style={styles.flex1}><Text>도로명</Text></View >
-                            <View style={styles.flex1}><Text>지번</Text></View >
-                        </View>
-                        <View style={styles.addressLayout}>
-                            <View style={styles.flex1}><Text style={{ color: "black" }}>{item.roadAddr} </Text></View >
-                            <View style={styles.flex1}><Text style={{ color: "black" }}>{item.jibunAddr}</Text></View >
-                        </View>
-                        <View style={styles.numberLayout}>
-                            <Text style={[styles.text,{fontWeight:'600'}]}>{item.zipNo}</Text>
+            <View style={styles.total_container}> 
+               <View style={styles.search_view}>
+                    <View style={styles.search_input}>
+                        <View style={styles.row_layout}>
+                            <TextInput style={styles.input}
+                                onChangeText={(text) => this.setState({ searchText: text  })}
+                                onEndEditing={this.searchAddress }
+                                placeholder="도로명 또는 지번을 입력하세요"
+                                placeholderTextColor="light grey" />
+                            <TouchableOpacity style={styles.search_btn} onPress={this.searchAddress}>
+                                <Icon name="search" size={30} color="light grey" />
+                            </TouchableOpacity>
                         </View>
                     </View>
-                </View></TouchableOpacity>}
-            />
-            </View>
-            <View style={[styles.viewBody,{position:'absolute', width:'100%', marginTop:'140%' }]}>
-                <View style={styles.rowLayout}>
-                    <TouchableOpacity onPress={this.pageDown} activeOpacity={0.8} >
-                       <PageIcon name="leftsquareo" size={30} color="light grey" />
-                    </TouchableOpacity>
+               </View>
+               <View style={styles.content_view}>
+               <Modal transparent={true} visible={this.state.indicator}>
+                    <Indicator />
+                </Modal>
+                    {this.state.searchViewVisible?
+                     <FlatList
+                        data={this.state.addressContents}
+                        renderItem={({item,index})=><AddressItem item={item} navigation={this.props.navigation} addressListener={this.props.route.params.addressListener}/>}/>:
+                        <>
+                        <Text style={styles.title}>TIP</Text>
+                        <Text style={styles.text}>도로명, 건물명, 지번 중 선택하여</Text>
+                        <Text style={styles.text2}>입력하세요 </Text>
+                        <Text style={styles.content}> 도로명 + 건물번호 <Text style={styles.content2}> 예) 테헤란로 152</Text></Text>
+                        <Text style={styles.content}> 동/읍/면/리 + 번지 <Text style={styles.content2}> 예) 역삼동 737</Text> </Text>
+                        <Text style={styles.content}> 건물명, 아파트명  <Text style={styles.content2}> 예) 삼성동 힐스테이트</Text></Text>
+                        </>}
                    
-                     <Text  style={styles.text}>   <Text style={[styles.text,{color:'blue'}]}>{this.state.page} </Text> / {Math.ceil(this.state.totalCount/4)}  </Text>
-                    <TouchableOpacity onPress={this.pageUp} activeOpacity={0.8}>
-                        <PageIcon name="rightsquareo" size={30} color="light grey" />
-                    </TouchableOpacity>
-                </View>
+               </View>
+                {this.state.searchViewVisible&&
+                <View style={styles.page_view}>
+                    <View style={styles.row_layout}>
+                        <TouchableOpacity onPress={this.pageDownClicked} activeOpacity={0.8} >
+                        <PageIcon name="leftsquareo" size={30} color="light grey" />
+                        </TouchableOpacity>
+                    
+                        <Text  style={styles.text}>   <Text style={[styles.text,{color:'blue'}]}>{this.state.page} </Text> / {Math.ceil(this.state.totalCount/4)}   </Text>
+                        <TouchableOpacity onPress={this.pageUpClicked} activeOpacity={0.8}>
+                            <PageIcon name="rightsquareo" size={30} color="light grey" />
+                        </TouchableOpacity>
+                    </View>
+                </View>}
             </View>
-            {console.log("hi")}
-            </>
         );
     }
 }
 
+class AddressItem extends PureComponent{
+    constructor(props) {
+        super(props);
+    }
+    addressItemClicked=(zipNo,roadAddr)=>{
+        this.props.navigation.navigate('Payment');
+        this.props.addressListener(zipNo, roadAddr);
+    }
+    render(){
+        const { zipNo,roadAddr,jibunAddr } = this.props.item;
+        return(
+        <TouchableOpacity activeOpacity={0.8} onPress={()=>this.addressItemClicked(zipNo,roadAddr)}>
+                <View style={styles.outputStyle}>
+                    <View style={styles.row_layout}>
+                        <View style={styles.titleLayout}>
+                            
+                            <View style={styles.flex}><Text>도로명</Text></View >
+                            <View style={styles.flex}><Text>지번</Text></View >
+                        </View>
+                        <View style={styles.addressLayout}>
+                            <View style={styles.flex}><Text style={{ color: "black" }}>{roadAddr} </Text></View >
+                            <View style={styles.flex}><Text style={{ color: "black" }}>{jibunAddr}</Text></View >
+                        </View>
+                        <View style={styles.numberLayout}>
+                            <Text style={[styles.text,{fontWeight:'600'}]}>{zipNo}</Text>
+                        </View>
+                    </View>
+                </View>
+        </TouchableOpacity>
+        );
+       
+    }
+}
 export default SearchAddress;
--- src/styles/address_search.js
+++ src/styles/address_search.js
@@ -1,119 +1,112 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet,Dimensions } from 'react-native';
+const ScreenHeight=Dimensions.get('window').height;
+const ScreenWidth=Dimensions.get('window').width;
 export const styles = StyleSheet.create({
   total_container:{
-    flex:1,
+    width:ScreenWidth,
+    height:ScreenHeight,
     backgroundColor:'white',
     
+    //borderWidth:1,
    },
-   container:{
-       flex:1,
-       marginTop:30,
-       marginLeft:15,
-       marginRight:15,
+   search_view:{
+    position:'absolute',
+    width:'100%',
+    paddingHorizontal:'7%',
+    paddingVertical:'5%'
+    //borderWidth:1,
    },
-    textInput:{
-       backgroundColor:'#F1F1F3',
-       marginBottom: 15,
-       paddingHorizontal: 5,
-       height: 55,
-       borderRadius: 10,
-       borderColor:'#F1F1F3',
-       //borderWidth: 1,
-    },
-    rowLayout: {
-        flex: 1,
-        flexDirection: 'row',
-        alignContent:'center',
-        justifyContent:'center'
-      },
-    flex1:{
-      flex:1,
-      marginBottom:5,
-      //borderWidth:1,
-    },
-    viewHeader:{
-      flex: 1,
-      
-    },
-    viewBody:{
-      //borderWidth:1,
-      flex: 9,
-      marginTop:25,
-    },
-    viewBottom:{
-      flex:1,
-      paddingTop:15,
-      //borderWidth:1,
-    },
-    inputStyle:{
-      borderColor:'#BDBDBD',
-      borderWidth:1,
-      borderRadius: 5,
-      height: 50,
-      paddingHorizontal:'2%'      
-    },
-    input:{
-      width:'90%',
-    
-      
-    },
-    search:{
-      alignContent:'center',
-      justifyContent:'center'
-    },
-    title: {
-      fontFamily: "bold",
-      fontSize: 20,
-      color: "black",
-      marginBottom: 15,
-    },
-
-    text:{
-      fontWeight: 'Cochin',
-      fontSize: 16,
-      marginBottom:4,
-      color: "black",
-    },
-    text2:{
-      fontWeight: 'Cochin',
-      fontSize: 18,
-      marginBottom:4,
-      color: "black",
-      marginBottom:20,
-    },
-    content:{
-      marginTop:5,
-      fontWeight: 'Cochin',
-      fontSize: 15,
-      color: "#747272",
-    },
-    content2:{
-      marginTop:5,
-      fontWeight: 'Cochin',
-      fontSize: 15,
-      color: "#9595E9",
-    },
-    outputStyle:{
-      borderColor:'#909098',
-      borderWidth:1,
-      height: 105,
-      paddingHorizontal:'2%'
-    },
-    titleLayout:{
-      flex:2.5,
-      //borderWidth:1,
-      flexDirection:'column'
-    },
-    addressLayout:{
-      flex:10,
-      paddingLeft:10,
-    },
-    numberLayout:{
-      flex:3,
-      paddingLeft:10,
-     
-      justifyContent:'center'
-    },
+   content_view:{
+    position:'absolute',
+    width:'100%',
+    top:ScreenHeight*0.13,
+    //borderWidth:1,
+    paddingHorizontal:'7%',
+   },
+   page_view:{
+    position:'absolute',
+    width:'100%',
+    top:ScreenHeight*0.8,
+    //borderWidth:1,
    
+   },
+   input:{
+    width:'90%'
+   },
+   search_input:{
+    borderColor:'#BDBDBD',  
+    borderWidth:1,
+    borderRadius: 5,
+    height: 45,
+    paddingHorizontal:'2%',
+          
+  },
+  row_layout: {
+    flexDirection: 'row',
+    alignContent:'center',
+    justifyContent:'center'
+  },
+  search_btn:{
+    //borderWidth:1,
+    //alignContent:'center',
+    justifyContent:'center'
+  },
+
+  title: {
+    fontFamily: "bold",
+    fontSize: 20,
+    color: "black",
+    marginBottom: 15,
+  },
+
+  text:{
+    fontWeight: 'Cochin',
+    fontSize: 16,
+    marginBottom:4,
+    color: "black",
+  },
+  text2:{
+    fontWeight: 'Cochin',
+    fontSize: 18,
+    marginBottom:4,
+    color: "black",
+    marginBottom:20,
+  },
+  content:{
+    marginTop:5,
+    fontWeight: 'Cochin',
+    fontSize: 15,
+    color: "#747272",
+  },
+  content2:{
+    marginTop:5,
+    fontWeight: 'Cochin',
+    fontSize: 15,
+    color: "#9595E9",
+  },
+
+  flex:{
+    flex:1,
+    marginBottom:5,
+    //borderWidth:1,
+  },
+  outputStyle:{
+    borderColor:'#909098',
+    borderWidth:1,
+    height: 105,
+    paddingHorizontal:'2%'
+  },
+  titleLayout:{
+    height:100,
+    flexDirection:'column'
+  },
+  addressLayout:{
+    flex:10,
+    paddingLeft:10,
+  },
+  numberLayout:{
+    flex:3,
+    paddingLeft:10,
+    justifyContent:'center'
+  },
 });
