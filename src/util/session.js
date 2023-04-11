4b6c72c9d6484f1af9701336561247e38b658ae5
DESKTOP-AI6RRTR\user <jiyumi00@naver.com>
2023-04-11 17:52:40 +0900

    Session 구조 변경

--- src/util/session.js
+++ src/util/session.js
@@ -1,23 +1,60 @@
+/*
+[userInfo:{
+    id: "1234567890",
+    companyName: "인제정비",
+    companyAddress: "인제로 인제대학교",
+    isLoggedin: true
+    },
+pageInfo:{
+    prevPage:"BuyList",
+    nextPage:"MyPage"
+    }
+]
+*/
 export default class Session {
-    static #items={};
-
-    static setValue(key,value) {
-        Session.#items[key]=value;
+    static #items=[];
+    
+    static setItem(key,item) {
+        Session.#items[key]=item;
     }
 
-    static getValue(key) {
+    static getItem(key) {
         return Session.#items[key];
     }
 
-    static setItem(item) {
-        Session.#items=item;
+    static setUserInfoItem(item) {
+        Session.#items['userInfo']=item;
+    }
+
+    static setPageInfoItem(item) {
+        Session.#items['pageInfo']=item;
+    }
+
+    static getUserInfoItem() {
+        return Session.#items['userInfo'];
+    }
+
+    static getPageInfoItem() {
+        return Session.#items['pageInfo'];
     }
 
     static isLoggedin() {
-        return Session.#items['isLoggedin'];
+        return Session.#items['userInfo'].isLoggedin;
     }
 
-    static getItem() {
-        return Session.#items;
+    static getUserID() {
+        return Session.#items['userInfo'].id;
     }
+
+    static getNextPage() {
+        return Session.#items['pageInfo'].nextPage;
+    }
+
+    static getPrevPage() {
+        return Session.#items['pageInfo'].prevPage;
+    }    
+
+/*     static setNextPage(value) {
+        Session.#items['pageInfo'].nextPage = value;
+    } */
 }
