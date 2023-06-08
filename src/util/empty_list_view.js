485a98278126a7eed52a6a71562e65d4fbc7162f
DESKTOP-AI6RRTR\user <jiyumi00@naver.com>
2023-06-08 20:05:06 +0900

    emtpy list style revise

--- src/util/empty_list_view.js
+++ src/util/empty_list_view.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView, RefreshControl } from 'react-native';
+import { View, Text, ScrollView, RefreshControl, Image, StyleSheet, Dimensions } from 'react-native';
 
-
+import { template, colors } from "../styles/template/page_style";
 import EmptyListIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default class EmptyListView extends Component {
@@ -13,22 +13,52 @@ export default class EmptyListView extends Component {
         return (
             <>
                 {this.props.hasOwnProperty('contentContainerStyle') && (<ScrollView
-                    style={{ borderWidth: 0, paddingTop: "45%", paddingLeft: "35%" }}
+                    style={{ borderWidth: 0, paddingTop: "15%" }}
                     refreshControl={<RefreshControl refreshing={this.props.isRefresh} onRefresh={this.props.onRefreshListener} />}
                     contentContainerStyle={this.props.contentContainerStyle}
                 >
-                    <EmptyListIcon name='clipboard-text-off-outline' size={100} />
-                    <Text style={{ fontSize: 18, }}>항목이 없습니다</Text>
+                    <View style={inStyle.iconView}>
+                        <Image
+                            style={{ width: 127, height: 62 }}
+                            source={
+                                require('../images/icon/empty-icon/empty.png')
+                            }
+                        />
+                        <Text style={[template.buttonText, { color: colors.black, marginTop: 10 }]}>항목이 없습니다</Text>
+                    </View>
+
                 </ScrollView>)}
-                
-                {this.props.hasOwnProperty('contentContainerStyle')==false && (<ScrollView
-                    style={{ borderWidth: 0, paddingTop: "45%", paddingLeft: "35%" }}
+
+                {this.props.hasOwnProperty('contentContainerStyle') == false && (<ScrollView
+                    style={{ borderWidth: 0, paddingTop: "15%", }}
                     refreshControl={<RefreshControl refreshing={this.props.isRefresh} onRefresh={this.props.onRefreshListener} />}
                 >
-                    <EmptyListIcon name='clipboard-text-off-outline' size={100} />
-                    <Text style={{ fontSize: 18, }}>항목이 없습니다</Text>
+                    <View style={inStyle.iconView}>
+                        <Image
+                            style={{ width: 127, height: 62 }}
+                            source={
+                                require('../images/icon/empty-icon/empty.png')
+                            }
+                        />
+                        <Text style={[template.buttonText, { color: colors.black, marginTop: 10 }]}>항목이 없습니다</Text>
+                    </View>
                 </ScrollView>)}
             </>
         );
     }
 }
+const ScreenHeight = Dimensions.get('window').height;
+const ScreenWidth = Dimensions.get('window').width;
+
+const inStyle = StyleSheet.create({
+    iconView: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.light,
+        width: ScreenWidth / 1.5,
+        height: ScreenWidth / 1.5,
+        borderRadius: 200,
+        alignSelf: 'center'
+    }
+
+}); 
